dc6fb8ce806de5bd39504bc9a755c5c9c8ee2a42
Chiawei Chien <142188887+memefather@users.noreply.github.com>
2023-09-24 00:59:38 -0700

    Update meme.py

--- meme.py
+++ meme.py
@@ -80,7 +80,7 @@ if options:
     headlines = {}
     radiohead = []
 
-    if all_articles['articles'] == None:
+    if all_articles['articles']['title'] == None:
         st.subheader("No news Today. Come Back Tomorrow!")
         st.markdown("![Alt Text](https://y.yarn.co/6b1e3a6f-f51e-492d-a48a-a40a27e3d471_text.gif)")
         st.stop()
