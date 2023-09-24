a584caaa4dafd707777b6cf66e6d2b8060357a68
Chiawei Chien <142188887+memefather@users.noreply.github.com>
2023-09-24 01:03:58 -0700

    Update meme.py

--- meme.py
+++ meme.py
@@ -83,8 +83,8 @@ if options:
     if all_articles['articles'] == []:
         st.subheader("No news Today. Come Back Tomorrow!")
         st.markdown("![Alt Text](https://y.yarn.co/6b1e3a6f-f51e-492d-a48a-a40a27e3d471_text.gif)")
-        st.write(all_articles)
         st.stop()
+    st.write(all_articles)
 
     for i in all_articles['articles']:
         headlines[i['title']] = i['title'] + '. ' + i['description']
